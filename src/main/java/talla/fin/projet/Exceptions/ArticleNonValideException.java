906da5e27f3c35b65333393b910f50a7139c34c7
Roland TALLA <102720955+RolandTapie@users.noreply.github.com>
2022-06-01 14:55:39 +0200

    Creation de l'exception sur le service Article

---
+++ src/main/java/talla/fin/projet/Exceptions/ArticleNonValideException.java
@@ -0,0 +1,8 @@
+package talla.fin.projet.Exceptions;
+
+public class ArticleNonValideException extends Exception {
+    public ArticleNonValideException(String message)
+    {
+        super(message);
+    }
+}
